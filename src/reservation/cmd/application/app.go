1e465d2b1e0f97682ec8d03dd18b0c94dae322c5
Alexander Kryshtop <kxcompare@gmail.com>
2023-10-30 22:09:04 +0300

    Initial commit for Reservation service

---
+++ src/reservation/cmd/application/app.go
@@ -0,0 +1,56 @@
+package application
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"reservation/cmd/configuration"
+	"syscall"
+
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+)
+
+type App struct {
+	Config *configuration.Config
+	Logger *zap.SugaredLogger
+}
+
+func New(cfg *configuration.Config, logger *zap.SugaredLogger) *App {
+	return &App{
+		Config: cfg,
+		Logger: logger,
+	}
+}
+
+func (a *App) Run() error {
+	server, err := a.newHTTPServer()
+	if err != nil {
+		return fmt.Errorf("new httpServer: %w", err)
+	}
+
+	wg, ctx := errgroup.WithContext(context.Background())
+	wg.Go(func() error {
+		return server.Run(ctx)
+	})
+
+	signalsCh := subscribeSignals()
+	wg.Go(func() error {
+		select {
+		case sig := <-signalsCh:
+			a.Logger.Infof("app received external signal for shutdown %v\n", sig)
+			return fmt.Errorf("shutdown by external signal %v", sig)
+		case <-ctx.Done():
+			return nil
+		}
+	})
+
+	return wg.Wait()
+}
+
+func subscribeSignals() <-chan os.Signal {
+	signalsCh := make(chan os.Signal, 1)
+	signal.Notify(signalsCh, syscall.SIGINT, syscall.SIGTERM)
+	return signalsCh
+}
