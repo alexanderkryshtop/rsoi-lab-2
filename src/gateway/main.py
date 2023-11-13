e8b6a516e437774a9446c56276bd6c47a0a57616
Alexander Kryshtop <kxcompare@gmail.com>
2023-11-13 21:47:53 +0300

    changes

--- src/gateway/main.py
+++ src/gateway/main.py
@@ -11,6 +11,14 @@ app.register_blueprint(library_app)
 def config_load():
     config_yaml = yaml.safe_load(open("config.yaml"))
     app.config["port"] = config_yaml["server"]["port"]
+    for service in config_yaml["services"]:
+        for key in service.keys():
+            service_name = key
+            hostname = service[key]["hostname"]
+            port = service[key]["port"]
+            url = f"http://{hostname}:{port}"
+            app.config[service_name] = url
+
 
 if __name__ == '__main__':
     config_load()
--- src/gateway/routes/library_routes.py
+++ src/gateway/routes/library_routes.py
@@ -1,9 +1,10 @@
+import requests
 from uuid import UUID
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
 
 from service.library_service import LibraryService
 
-library_app = Blueprint("library", __name__, url_prefix="/libraries")
+library_app = Blueprint("library", __name__, url_prefix="/api/v1/libraries")
 
 library_service = LibraryService()
 
@@ -12,10 +13,15 @@ def get_libraries_with_pagination():
     page = request.args.get("page")
     size = request.args.get("size")
     city = request.args.get("city")
+    if not city:
+        return "", 400
 
-    libs = library_service.get_libraries(city, page, size)
+    decoded_query_string = request.query_string.decode()
+    prefix = current_app.config['library']
+    url = f"{prefix}/libraries?{decoded_query_string}"
+    result = requests.get(url)
 
-    return f"{libs}", 200
+    return result.text, result.status_code
 
 @library_app.route("/<library_uid>/books")
 def get_books_in_library(library_uid: str):
