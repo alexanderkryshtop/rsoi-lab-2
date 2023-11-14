2c2453cf25e4e1d7633da9c6544af2f0c248b2fe
Alexander Kryshtop <kxcompare@gmail.com>
2023-11-14 08:19:59 +0300

    add rating handler

--- src/rating/routes/rating_routes.py
+++ src/rating/routes/rating_routes.py
@@ -8,7 +8,7 @@ rating_app = Blueprint("rating", __name__, url_prefix="/rating")
 rating_service = RatingService()
 
 @rating_app.route("/")
-def get_libraries_with_pagination():
+def get_rating():
     username = request.headers.get("X-User-Name")
     user_stars_count = rating_service.get_star_count(username)
 
@@ -16,4 +16,16 @@ def get_libraries_with_pagination():
         "stars": user_stars_count
     }
 
+    return response, 200
+
+@rating_app.route("/change", methods=["POST"])
+def change_rating():
+    username = request.headers.get("X-User-Name")
+    delta = request.json["delta"]
+
+    user_stars_count = rating_service.change_star_count(username, delta)
+    response = {
+        "stars": user_stars_count
+    }
+    
     return response, 200
--- src/rating/service/rating_service.py
+++ src/rating/service/rating_service.py
@@ -9,3 +9,16 @@ class RatingService:
         if not ratingModel:
             return None
         return ratingModel.stars
+
+    def change_star_count(self, username: str, delta: int) -> Optional[int]:
+        ratingModel: RatingModel = RatingModel.query.filter(RatingModel.username == username).one_or_none()
+        if not ratingModel:
+            return None
+        new_rating = ratingModel.stars + delta
+        if new_rating >= 100:
+            new_rating = 100
+        if new_rating <= 1:
+            new_rating = 1
+        ratingModel.stars = new_rating
+        RatingModel.query.session.commit()
+        return ratingModel.stars
